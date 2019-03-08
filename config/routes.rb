242e1fdacd79ec52b9e2964bda188440c576ab7b
Kiseki Shoji <o9o3.kiseki@gmail.com>
2019-03-08 23:02:24 +0900

    add routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
 
-  get '/' => 'pages#top'
+  get '/' => 'blogs#index'
 
   devise_for :users, controllers: {
     registrations: 'users/registrations'
   }
 
-  resources :users
+  resources :blogs
 end
