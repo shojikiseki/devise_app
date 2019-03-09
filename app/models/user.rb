8840beddbe8a67cf1e97b44d446158998cb8dd91
Kiseki Shoji <o9o3.kiseki@gmail.com>
2019-03-09 14:16:22 +0900

    add association

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,6 @@ class User < ApplicationRecord
 
   validates :name, presence: true
   has_many :blogs
+  has_many :favorites, dependent: :destroy
+  has_many :favorite_blogs, through: :favorites, source: :blog
 end
